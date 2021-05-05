6d74cad980d025162149a3f034730f194f8ebc6c
Sofiia <65614017+Wozniak456@users.noreply.github.com>
2021-05-05 23:28:24 +0300

    Add files via upload
    c# program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Laba6_OOP_Lib;
+namespace Laba6_OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Vozniak Sofiia, is-01, 2 Variant");
+            //x1 = 0, y1 = 0, x2 = 4, y2 = 4, x3 = 6, y3 = 0
+            Triangles trianle = new Triangles(0, 0, 4, 4, 6, 0); 
+            trianle.Perimetr();
+            trianle.Square();
+            //радіус = 3
+            Circle circle = new Circle(3);
+            circle.Perimetr();
+            circle.Square();
+        }
+    }
+}
